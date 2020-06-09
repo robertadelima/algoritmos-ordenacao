3e789ddeff570ab4d4852ace874ea9a84ac87d1a
robertadelima <robertarl@gmail.com>
2020-06-09 17:51:15 -0300

    deleted unused comments and kept original writing order

--- src/ordenacao/Ordenacao.java
+++ src/ordenacao/Ordenacao.java
@@ -9,14 +9,6 @@ public class Ordenacao {
 
     public static void main(String[] args) {
 
-//        BubbleSort bs = new BubbleSort();
-//        InsertionSort is = new InsertionSort();
-//        SelectionSort ss = new SelectionSort();
-//        MergeSort ms = new MergeSort();
-//        QuickSort qs = new QuickSort();
-//        ShellSort shs = new ShellSort();
-        
-        //Novo
         ArrayList<MetodoDeOrdenacao> metodosOrdenacao = new ArrayList<>();
         metodosOrdenacao.add(new BubbleSort());
         metodosOrdenacao.add(new InsertionSort());
@@ -48,55 +40,29 @@ public class Ordenacao {
         	ordemDecrescente = vertorEmOrdemDecrescente(tamanho);
         	ordemAleatoria = vertorEmDesordem(tamanho);
         	
+        	//Dessa maneira para obter todos os métodos com input crescente, seguido de todos os métodos por decrescente
+        	//e todos os métodos com input aleatório
         	for(MetodoDeOrdenacao metodo : metodosOrdenacao) {
         		 metodo.setVetor(ordemCrescente.clone()); 
         		 metodo.sort(repeticoes); 
         		 metodo.writeToFile("Crescente", metodo.getClass().getSimpleName());
         	}
+        	
         	for(MetodoDeOrdenacao metodo : metodosOrdenacao) {
-       		 metodo.setVetor(ordemDecrescente.clone()); 
-       		 metodo.sort(repeticoes); 
-       		 metodo.writeToFile("Decrescente", metodo.getClass().getSimpleName());
+       		 	metodo.setVetor(ordemDecrescente.clone()); 
+       		 	metodo.sort(repeticoes); 
+       		 	metodo.writeToFile("Decrescente", metodo.getClass().getSimpleName());
         	}
+        	
         	for(MetodoDeOrdenacao metodo : metodosOrdenacao) {
-       		 metodo.setVetor(ordemAleatoria.clone()); 
-       		 metodo.sort(repeticoes); 
-       		 metodo.writeToFile("Aleatoria", metodo.getClass().getSimpleName());
+       		 	metodo.setVetor(ordemAleatoria.clone()); 
+       		 	metodo.sort(repeticoes); 
+       		 	metodo.writeToFile("Aleatoria", metodo.getClass().getSimpleName());
         	}
         }
         
-        
-//        int tamanho = 100;
-//        for(int i = 1; i<=3; i++){
-//            ordemCrescente = vertorEmOrdemCrescente(tamanho);
-//            ordemDecrescente = vertorEmOrdemDecrescente(tamanho);
-//            ordemAleatoria = vertorEmDesordem(tamanho);
-//            
-//            System.out.println("VETOR COM "+tamanho+" ELEMENTOS EM ORDEM CRESCENTE - MELHOR CASO");
-//            bs.setVetor(ordemCrescente.clone()); bs.sort(repeticoes); bs.print(); 
-//            is.setVetor(ordemCrescente.clone()); is.sort(repeticoes); is.print(); 
-//            ss.setVetor(ordemCrescente.clone()); ss.sort(repeticoes); ss.print(); 
-//            ms.setVetor(ordemCrescente.clone()); ms.sort(repeticoes); ms.print(); 
-//            qs.setVetor(ordemCrescente.clone()); qs.sort(repeticoes); qs.print(); 
-//            shs.setVetor(ordemCrescente.clone()); shs.sort(repeticoes); shs.print();
-//            System.out.println("VETOR COM "+tamanho+" ELEMENTOS EM ORDEM DECRESCENTE - PIOR CASO");
-//            bs.setVetor(ordemDecrescente.clone()); bs.sort(repeticoes); bs.print(); 
-//            is.setVetor(ordemDecrescente.clone()); is.sort(repeticoes); is.print(); 
-//            ss.setVetor(ordemDecrescente.clone()); ss.sort(repeticoes); ss.print(); 
-//            ms.setVetor(ordemDecrescente.clone()); ms.sort(repeticoes); ms.print(); 
-//            qs.setVetor(ordemDecrescente.clone()); qs.sort(repeticoes); qs.print(); 
-//            shs.setVetor(ordemDecrescente.clone()); shs.sort(repeticoes); shs.print();     
-//            System.out.println("VETOR COM "+tamanho+" ELEMENTOS EM DESORDEM");
-//            bs.setVetor(ordemAleatoria.clone()); bs.sort(repeticoes); bs.print(); 
-//            is.setVetor(ordemAleatoria.clone()); is.sort(repeticoes); is.print(); 
-//            ss.setVetor(ordemAleatoria.clone()); ss.sort(repeticoes); ss.print(); 
-//            ms.setVetor(ordemAleatoria.clone()); ms.sort(repeticoes); ms.print(); 
-//            qs.setVetor(ordemAleatoria.clone()); qs.sort(repeticoes); qs.print(); 
-//            shs.setVetor(ordemAleatoria.clone()); shs.sort(repeticoes); shs.print();      
-//            tamanho = tamanho * 10;        
-//        }
-//    
     }
+    
     
     public static int[] vertorEmOrdemCrescente(int tamanho){
         int[] vetor = new int[tamanho];
